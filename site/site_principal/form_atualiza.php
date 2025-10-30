8a508725f8c6b8f4bf3e56e1ed512e35be8a4aa9
Caiooliveirabarreto <caiooliveirabarreto4@gmail.com>
2025-10-30 12:02:48 -0300

    Esse forms é nosso sofrimento e disgraça

--- site/site_principal/form_atualiza.php
+++ site/site_principal/form_atualiza.php
@@ -8,9 +8,10 @@
             require '../conexao.php';
             $id = $_GET['id'];
             // $id = str_replace("type=","", $id);
-            $sql = "SELECT * FROM item WHERE cod_item = $id";
+            $sql = "SELECT * FROM item WHERE Cod_item = $id";
             $stmt = $pdo->query($sql);
             $Item = $stmt->fetch(PDO::FETCH_ASSOC);
+
         ?>  
             <div class="mb-3">
                 Nome: <input type="text" value="<?php echo $Item['Nome']; ?>" name="nome_novo" class="form-control">
@@ -18,6 +19,7 @@
             <div class="mb-3">
                 Preço: <input type="text" value="<?php echo $Item['Preco']; ?>" name="preco_novo" class="form-control">
             </div>
+
             <div class="mb-3">
                 Quantidade: <input type="text" value="<?php echo $Item['Quantidade_Estoque']; ?>" name="quantidade_novo" class="form-control">
             </div>
@@ -27,8 +29,14 @@
             <div class="mb-3">
                 Descrição: <input type="text" value="<?php echo $Item['Descricao']; ?>" name="descricao_nova" class="form-control">
             </div>
-            
-            <button type="submit" class="btn btn-danger">ATUALIZAR</button> 
+
+            <button type="button" name="btn-atualiza" class="btn btn-danger">ATUALIZAR</button>
+            <?php 
+                if (isset($_POST['btn-atualiza'])){
+                    echo 'teste';
+                    header('location: diretor.php');
+                }
+            ?>
         </form>
     </div>
 
